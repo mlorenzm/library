277fd598e941b4ab9caa357b5ae57340c33163ca
mlorenzm <mlorenzomohamed@gmail.com>
2023-04-14 18:56:58 +0100

    functionality for adding cards dynamically based on library, delete prompt to add to library and changing layout system to main container

--- src/app.js
+++ src/app.js
@@ -16,17 +16,32 @@ function addToLibrary(bookObject){
 };
 
 function displayLibrary(library){
-    const clickmessage = document.getElementById('click-to-add');
-    clickmessage.classList.add('hidden');
+    deleteElementPrompt();
     library.forEach(book => {
-        const main = document.getElementById('main');
-        let card = document.createElement('div');
-        let title = document.createElement('div');
-        let author = document.createElement('div');
-        let pages = document.createElement('div');
-        let read = document.createElement('div');
-        card.append(title, author, pages, read);
-        main.append(card);
+        createCards(book);
     });
 }
+const createCards = (book) => {
+    const main = document.getElementById('main');
+    main.classList.add('flex-wrap', 'h-10', 'gap-3');
+    let card = document.createElement('div');
+    card.classList.add('rounded', 'shadow-lg', 'p-3', 'border-l-8', 'border-l-slate-500', 'flex', 'flex-col', 'gap-1');
+    let title = document.createElement('div');
+    title.innerText = book.title;
+    title.classList.add('text-3xl', 'font-black');
+    let author = document.createElement('div');
+    author.innerText = book.author;
+    author.classList.add('text-slate-500', 'text-lg', 'italic');
+    let pages = document.createElement('div');
+    pages.innerText = book.pages;
+    let read = document.createElement('button');
+    read.classList.add('bg-slate-700', 'font-black', 'p-2', 'rounded-lg', 'text-slate-50', 'hover:bg-slate-600', 'cursor-pointer');
+    read.innerText = book.read
+    card.append(title, author, pages, read);
+    main.append(card);
+}
 
+const deleteElementPrompt = () => {
+    const clickmessage = document.getElementById('click-to-add');
+    clickmessage.classList.add('hidden');
+};
