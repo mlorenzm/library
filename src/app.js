362983ffe22c1ad3917dd129d0b373190c14e364
mlorenzm <mlorenzomohamed@gmail.com>
2023-04-15 13:37:51 +0100

    add to library functionality

--- src/app.js
+++ src/app.js
@@ -10,21 +10,31 @@ function Book(title, author, pages, read) {
 let hobbit = new Book('Hobbit', 'tolkien', '295', false);
 let potter = new Book('Potter', 'mary', '300', false);
 let habits = new Book('atomic Habits', 'mary', '300', true);
-let myLibrary = [];
+
+
+let myLibrary = [hobbit, potter, potter];
+
 
 function addToLibrary(bookObject){
     myLibrary.push(bookObject);
 };
 
 function displayLibrary(library){
-    deleteElementPrompt();
+    if(library.length != 0){
+        // deleteElementPrompt();
+    }
+    //  main = document.getElementById('main');
+     const main = document.getElementById('main');
+     main.classList.add('flex-wrap', 'gap-4');
+     while (main.firstChild) {
+         main.firstChild.remove();
+       }
+    
     library.forEach(book => {
         createCards(book);
     });
-}
+};
 const createCards = (book) => {
-    const main = document.getElementById('main');
-    main.classList.add('flex-wrap', 'h-10', 'gap-4');
     let card = document.createElement('div');
     card.classList.add('rounded', 'shadow-lg', 'p-3', 'border-l-8', 'border-l-slate-500', 'flex', 'flex-col', 'gap-1');
     let title = document.createElement('div');
@@ -34,43 +44,60 @@ const createCards = (book) => {
     author.innerText = book.author;
     author.classList.add('text-slate-500', 'text-lg', 'italic');
     let pages = document.createElement('div');
-    pages.innerText = book.pages;
+    pages.innerText = `${book.pages} pages`;
     let read = document.createElement('button');
     read.classList.add('font-black', 'p-2', 'rounded-lg', 'text-slate-50', 'hover:scale-101', 'cursor-pointer', 'shadow-lg');
-    read.innerText = 'ok?'
     if(book.read){
         read.innerText = 'Mark as unread';
-        read.classList.add('bg-red-400');
+        read.classList.add('bg-red-500');
     } else{
         read.innerText = 'Mark as read';
-        read.classList.add('bg-green-400');
+        read.classList.add('bg-green-500');
     }
+    card.id = 'card';
     card.append(title, author, pages, read);
+    console.log(card)
     main.append(card);
 }
 
-const deleteElementPrompt = () => {
-    const clickmessage = document.getElementById('click-to-add');
-    clickmessage.classList.add('hidden');
-};
+// const deleteElementPrompt = () => {
+//     const clickmessage = document.getElementById('click-to-add');
+//     clickmessage.classList.add('hidden');
+// };
+
 const toggleForm = () => {
     const bookForm = document.getElementById('form-container');
     if(bookForm.classList.contains('hidden')){
-        bookForm.classList.remove('hidden');    
+        bookForm.classList.remove('hidden');     
     } else{
         bookForm.classList.add('hidden');
     }
 };
+const addButton = document.getElementById('btn');
+addButton.addEventListener('click', () =>{
+    toggleForm();
+});
+
 
 const addBook = () => {
-    const addButton = document.getElementById('btn');
-    
-    addButton.addEventListener('click', () =>{
-        toggleForm();
-        console.log('yes');
-    })
-}
+        let bookTitle = document.getElementById('title').value;
+        let bookAuthor = document.getElementById('author').value;
+        let bookPages = document.getElementById('pages').value;
+        let readValue = document.querySelector('input[name="read-status"]:checked').value;
+        if (readValue == 'true'){
+            readValue = true
+        } else {readValue = false}
+        let individualBook = new Book(bookTitle, bookAuthor, bookPages, readValue);
+        myLibrary.push(individualBook);
+        console.log(myLibrary.length)
+        displayLibrary(myLibrary);
+        toggleForm()
+    }
+
+const addToLibraryBtn = document.getElementById('add-to-library');
+addToLibraryBtn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    addBook();
+})
 
 
-addBook();
-displayLibrary(myLibrary);
